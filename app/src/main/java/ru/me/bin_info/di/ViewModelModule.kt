c6471f5f6503fd0f60662a06e7fb46086cd27175
ber9z <berez.a@bk.ru>
2025-02-14 20:03:30 +0300

    добавление viewmodel в di

---
+++ app/src/main/java/ru/me/bin_info/di/ViewModelModule.kt
@@ -0,0 +1,11 @@
+package ru.me.bin_info.di
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import ru.me.bin_info.presentation.SearchViewModel
+
+val viewModelModel = module {
+    viewModel<SearchViewModel> {
+        SearchViewModel()
+    }
+}
--- app/src/main/java/ru/me/bin_info/util/App.kt
+++ app/src/main/java/ru/me/bin_info/util/App.kt
@@ -4,13 +4,15 @@ import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import ru.me.bin_info.di.dataModule
+import ru.me.bin_info.di.repositoryModule
+import ru.me.bin_info.di.viewModelModel
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidContext(this@App)
-            modules(dataModule)
+            modules(dataModule, repositoryModule, viewModelModel)
         }
     }
 }
